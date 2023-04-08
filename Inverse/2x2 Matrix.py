32de6b8b7d38c57d0313a934585d0e78745796ef
Pseudonym <125481007+hanyPy@users.noreply.github.com>
2023-04-07 22:54:46 -0600

    Update 2x2 Matrix.py

--- Inverse/2x2 Matrix.py
+++ Inverse/2x2 Matrix.py
@@ -1,24 +1,48 @@
-# The inverse of a 2x2 matrix
-# 3rd March 2023
+'''
+The determinant and inverse of a 2x2 matrix.
+Written on 3rd March, 2023.
+Last edit on 7th April, 2023.
+'''
 
-num1 = float(input("Enter a number: "))
-num2 = float(input("Enter a number: "))
-num3 = float(input("Enter a number: "))
-num4 = float(input("Enter a number: "))
+det= ()
+def matrix(det):
+    try:   
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter a number: "))
+        num3 = float(input("Enter a number: "))
+        num4 = float(input("Enter a number: "))
+    except TypeError  as e:
+                print(e)
+                print('You entered a string, please re-rerun the program')
+    except ValueError  as a:
+                print(a)
+                print('You entered a string, please re-rerun the program')
+                
+    #finding and printing the determinant.    
+    det = ((num1*num4) - (num2*num3))
+    print("\nThe determinant is " + str(det) + ".\n")
+      
+    try:
+        c11 = num1/det
+        c12 = num2/det
+        c21 = num3/det
+        c22 = num4/det
+        
+    # Just incase the determinant is 0.
+    except ZeroDivisionError:
+        print("Since the determinant is 0, dividing the numbers isn't possible. But anyway, this is their disjoint order.")
+        Ifzero1 = [num4, -num2]
+        Ifzero2 = [-num3, num1]
+        print(Ifzero1)
+        print(Ifzero2)
+        
+    #We switch c11 and c22 , and multiply c21 and c12 with -1
+    try:
+        Inverse1 = [c22, -c12]
+        Inverse2 = [-c21, c11]
+        print(Inverse1)
+        print(Inverse2)
+    except:
+        pass
 
-# finding the determinant  (ad-bc)
-det = ((num1*num4) - (num2*num3))
-
-print("The determinant is: " + str(det))
-
-c11 = num1/det
-c12 = num2/det
-c21 = num3/det
-c22 = num4/det
-
-#We switch c11 and c22 , and multiply c21 and c12 with -1
-
-Inverse=  [c22 , -c12]
-inverse2= [-c21 , c11]
-print(Inverse)
-print(inverse2)
+matrix(det)
